a0176900a7ee8be27a6e5f1d3b66f1659ae5cccb
Onur İrken <oirken@protel.com.tr>
2019-06-25 13:25:17 +0300

    Prod configuration

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import resolvers from "./resolvers";
 import models, { sequelize } from "./models";
 import loaders from "./loaders";
 const app = express();
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 
@@ -71,14 +72,15 @@ const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
 
 const isTest = !!process.env.TEST_DATABASE;
+const isProduction = !!process.env.DATABASE_URL;
 
-sequelize.sync({ force: isTest }).then(async () => {
-  if (isTest) {
+sequelize.sync({ force: isTest || isProduction }).then(async () => {
+  if (isTest || isProduction) {
     createUsersWithMessages(new Date());
   }
 
-  httpServer.listen({ port: 8000 }, () => {
-    console.log("Apollo Server on http://localhost:8000/graphql");
+  httpServer.listen({ port }, () => {
+    console.log(`Apollo Server on http://localhost:${port}/graphql`);
   });
 });
 
